37a014fb5d8652374af5a8972d85d694b95f3ad2
AndrewYW <andrewwang39@gmail.com>
2016-10-14 15:58:16 -0400

    add icon and css

--- src/application/MainApp.java
+++ src/application/MainApp.java
@@ -7,6 +7,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -53,7 +54,7 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
-
+        this.primaryStage.getIcons().add(new Image("file:resources/images/addressbook.png"));
         initRootLayout();
 
         showPersonOverview();
@@ -119,6 +120,7 @@ public class MainApp extends Application {
             // Create the dialog Stage.
             Stage dialogStage = new Stage();
             dialogStage.setTitle("Edit Person");
+            dialogStage.getIcons().add(new Image("file:resources/images/addressbook.png"));
             dialogStage.initModality(Modality.WINDOW_MODAL);
             dialogStage.initOwner(primaryStage);
             Scene scene = new Scene(page);
