70cd192b1ccaea893a46fe2f9d0010d14f01d339
AndrewYW <andrewwang39@gmail.com>
2016-10-14 16:50:54 -0400

    add some xml data handling methods and update menu options

--- src/application/MainApp.java
+++ src/application/MainApp.java
@@ -1,18 +1,27 @@
 package application;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.prefs.Preferences;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.Person;
+import model.PersonListWrapper;
 import view.PersonEditDialogController;
 import view.PersonOverviewController;
 
@@ -151,5 +160,88 @@ public class MainApp extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    /**
+     * Returns the person file preference, i.e. the file that was last opened.
+     * The preference is read from the OS specific registry. If no such
+     * preference can be found, null is returned.
+     * 
+     * @return
+     */
+    public File getPersonFilePath() {
+    	Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+    	String filePath = prefs.get("filepath", null);
+    	if(filePath != null) {
+    		return new File(filePath);
+    	} else {
+    		return null;
+    	}
+    }
+    
+    public void setPersonFilePath(File file) {
+    	Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+        if (file != null) {
+            prefs.put("filePath", file.getPath());
+
+            // Update the stage title.
+            primaryStage.setTitle("AddressApp - " + file.getName());
+        } else {
+        	prefs.remove("filePath");
+        	
+        	primaryStage.setTitle("AddressApp");
+        }
+    }
+    
+    /**
+     * Load person data from specified file. Replaces current person data.
+     * @param file
+     */
+    public void loadPersonDataFromFile(File file) {
+    	try {
+    		JAXBContext context = JAXBContext
+    				.newInstance(PersonListWrapper.class);
+    		Unmarshaller um = context.createUnmarshaller();
+    		
+    		PersonListWrapper wrapper = (PersonListWrapper) um.unmarshal(file);
+    		
+    		personData.clear();
+    		personData.addAll(wrapper.getPersons());
+    		
+    		setPersonFilePath(file);
+    	} catch (Exception e) {
+    		Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Could not load data");
+            alert.setContentText("Could not load data from file:\n" + file.getPath());
+
+            alert.showAndWait();
+    	}
+    }
+    
+    public void savePersonDataToFile(File file) {
+    	try {
+    		JAXBContext context = JAXBContext
+                    .newInstance(PersonListWrapper.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            // Wrapping our person data.
+            PersonListWrapper wrapper = new PersonListWrapper();
+            wrapper.setPersons(personData);
+
+            // Marshalling and saving XML to the file.
+            m.marshal(wrapper, file);
+
+            // Save the file path to the registry.
+            setPersonFilePath(file);
+    	} catch (Exception e) {
+    		Alert alert = new Alert(AlertType.ERROR);
+	        alert.setTitle("Error");
+	        alert.setHeaderText("Could not save data");
+	        alert.setContentText("Could not save data to file:\n" + file.getPath());
+
+	        alert.showAndWait();
+    	}
     }
 }
